df579676ffb0e94771ce4675e70ef138dc785120
Andrew Yolland <and.t.yoll@gmail.com>
2015-02-12 18:07:00 -0600

    Refactored display to get search to work.

--- controller.rb
+++ controller.rb
@@ -16,17 +16,19 @@ end
 
 get "/products" do
   @objects = Product.seek_all
-  binding.pry
+  @type = "Product"
   erb :products
 end
 
 get "/categories" do
   @objects = Category.seek_all
+  @type = "Category"
   erb :categories
 end
 
 get "/locations" do
   @objects = Location.seek_all
+  @type = "Location"
   erb :locations
 end
 
@@ -46,8 +48,7 @@ get "/mark" do
   object_name = params[:type].capitalize
   a = Object.const_get(object_name).send("new",params)
   a.id = "XX" + a.id.to_s + "XX"
-  a.cram
-  swoosh
+  swoosh(a)
 end
 
 get "/edit" do
@@ -63,9 +64,7 @@ get "/new/:type" do
   object_name = params[:type].capitalize
   a = Object.const_get(object_name).send("new",params)
   a.id = params[:id].to_i if a.id.to_i != 0
-  valid_loc_cat(a)
-  a.cram
-  swoosh
+  swoosh(a)
 end
 
 get "/search" do
@@ -74,34 +73,47 @@ end
 
 helpers do
   
-  def swoosh
-    if params[:type] == "Product"
+  def swoosh(object)
+    object.cram if valid_loc_cat(object)
+    if params[:type] == "Product" && valid_loc_cat(object)
       @objects = Product.seek_all
-      binding.pry
       erb :products
     elsif params[:type] == "Category"
       @objects = Category.seek_all
       erb :categories
-    else params[:type] == "Location"
+    elsif params[:type] == "Location"
       @objects = Location.seek_all
       erb :locations
+    else
+      erb :invalid
     end
   end
   
   def valid_loc_cat(object)
     match_loc = WAREHOUSE.execute("SELECT * FROM locations WHERE id =#{object.location_id}")
     match_cat = WAREHOUSE.execute("SELECT * FROM categories WHERE id =#{object.category_id}")
-    binding.pry
-    if (object.location_id != nil || object.category_id != nil)&&(match_loc != [] || match_cat != [])
-      erb :invalid
-    end
+    (object.location_id == nil || object.category_id == nil)&&(match_loc == [] || match_cat == [])
   end
   
   def loc_cat_id(object)
-    #binding.pry
     category_name = WAREHOUSE.execute("SELECT name FROM categories WHERE id = #{object.category_id}")[0]["name"]
     location_name = WAREHOUSE.execute("SELECT name FROM locations WHERE id = #{object.location_id}")[0]["name"]
     return location_name, category_name
+  end
+  
+  def index
+    index = "id".ljust(5)+"category".ljust(15)+"location" if @type == "Product"
+    index = "id".ljust(5)+"name".ljust(15)+"cost".ljust(6)+"description" if @type == "Category"
+    index = "id".ljust(5)+"name".ljust(15)+"capacity" if @type == "Location"
+    index
+  end
+  
+  def entry(object)
+    location_name, category_name = loc_cat_id(object) if @type == "Product"
+    entry = (object.id.to_s.ljust(5) + category_name.ljust(15) + location_name) if @type == "Product"
+    entry = (object.id.to_s.ljust(5) + object.name.ljust(15) + object.cost.to_s.ljust(6) + object.description) if @type == "Category"
+    entry = (object.id.to_s.ljust(5) + object.name.ljust(15) + object.capacity.to_s) if @type == "Location"
+    return entry
   end
   
   def plural(type)
